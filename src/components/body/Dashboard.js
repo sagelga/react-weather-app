b08ca04ec779f96d1932879a171c864575db10de
Kunanon Srisuntiroj <son9912@gmail.com>
2023-05-02 18:54:48 +0700

    Simplifying component structure

--- src/components/body/Dashboard.js
+++ src/components/body/Dashboard.js
@@ -1,10 +1,17 @@
 import React from 'react'
-import CardGallery from './CardGallery'
+// import Container from '@mui/icons-material/Container'
+import BasicCard from './BasicCard'
 
 function Dashboard() {
+    // For each row, there will be only 3 cards per row
+    // Each card will contains the title icon, title, main value, and description
     return (
         <div className="dashboard">
-            <CardGallery />
+            {/* <Container> */}
+            <BasicCard />
+            <BasicCard />
+            <BasicCard />
+            {/* </Container> */}
         </div>
     )
 }
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,12 +2,10 @@ import React from 'react'
 
 function Footer() {
     return (
-        <footer>
-            <p>
-                Weather app made in React.JS © 2023 by{' '}
-                <a href="https://github.com/sagelga">@sagelga</a>
-            </p>
-        </footer>
+        <p>
+            Weather app made in React.JS © 2023 by{' '}
+            <a href="https://github.com/sagelga">@sagelga</a>
+        </p>
     )
 }
 
