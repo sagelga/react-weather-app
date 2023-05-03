a26356f42973a68c4c832d966b6866716a3ac75d
Kunanon Srisuntiroj <son9912@gmail.com>
2023-05-03 16:08:25 +0700

    Cover TemperatureBody with Container

--- src/components/body/TemperatureBody.js
+++ src/components/body/TemperatureBody.js
@@ -1,20 +1,25 @@
 import React from 'react'
+import Container from '@mui/material/Container'
 
 function TemperatureBody(props) {
     return (
-        <div className="body-main">
-            <div className="body-temperature-main-card">
-                <h2 className="city">in {props.weatherData.name}, it's</h2>
-                <h1 className="current">
-                    {props.weatherData.main.temp} C° with{' '}
-                    {props.weatherData.weather[0].description}
-                </h1>
-                <h2>
-                    Feels like {props.weatherData.main.feels_like} C° with{' '}
-                    {props.weatherData.main.humidity}% humidity
-                </h2>
+        <Container>
+            <div className="body-main">
+                <div className="body-temperature-main-card">
+                    <h2 className="city">
+                        Currently in {props.weatherData.name}, it's
+                    </h2>
+                    <h1 className="current">
+                        {props.weatherData.main.temp} C° with{' '}
+                        {props.weatherData.weather[0].description}
+                    </h1>
+                    <h2>
+                        Feels like {props.weatherData.main.feels_like} C° with{' '}
+                        {props.weatherData.main.humidity}% humidity
+                    </h2>
+                </div>
             </div>
-        </div>
+        </Container>
     )
 }
 export default TemperatureBody
