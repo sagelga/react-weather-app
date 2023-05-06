dc2d13006e6a7ea26b10d6e59219e55aea4e1c02
Kunanon Srisuntiroj <son9912@gmail.com>
2023-05-06 15:38:59 +0700

    Refactor text variables to top line

--- src/components/body/TemperatureBody.js
+++ src/components/body/TemperatureBody.js
@@ -2,20 +2,31 @@ import React from 'react'
 import Container from '@mui/material/Container'
 
 function TemperatureBody(props) {
+    // Building variables from props and passing it to component
+    const locationName = props.weatherData.name
+    const temperature = props.weatherData.main.temp
+    const weatherCondition = props.weatherData.weather[0].description
+    const feelsLike = props.weatherData.main.feels_like
+    const humidity = props.weatherData.main.humidity
+
+    const metric = 'C°'
+
+    // If there is no value for the name, return an empty div
+    if (props.weatherData.name === null) {
+        return <div></div>
+    }
+
     return (
         <Container>
             <div className="body-main">
                 <div className="body-temperature-main-card">
-                    <h2 className="city">
-                        Currently in {props.weatherData.name}, it's
-                    </h2>
+                    <h2 className="city">Currently in {locationName}, it's</h2>
                     <h1 className="current">
-                        {props.weatherData.main.temp} C° with{' '}
-                        {props.weatherData.weather[0].description}
+                        {temperature} {metric} with {weatherCondition}
                     </h1>
                     <h2>
-                        Feels like {props.weatherData.main.feels_like} C° with{' '}
-                        {props.weatherData.main.humidity}% humidity
+                        Feels like {feelsLike} {metric} with {humidity}%
+                        humidity
                     </h2>
                 </div>
             </div>
