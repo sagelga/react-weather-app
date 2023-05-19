242c2ad1c74379c73d2c5dfd33ebacb2000635c1
Kunanon Srisuntiroj <son9912@gmail.com>
2023-05-19 22:08:55 +0700

    Add mockup UI for forecast

--- src/components/body/ForecastCard.js
+++ src/components/body/ForecastCard.js
@@ -1,5 +1,31 @@
 import React from 'react'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import Typography from '@mui/material/Typography'
 
-export default ForecastCard = () => {
-    return <div>ForecastCard</div>
+const ForecastCard = () => {
+    return (
+        <div>
+            <Card>
+                <CardContent>
+                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                        09:00
+                    </Typography>
+                    <img
+                        src="https://img.icons8.com/fluency/32/null/rain"
+                        className="center"
+                        alt="Current weather"
+                    ></img>
+                    <Typography variant="h5" component="div" className="center">
+                        28
+                    </Typography>
+                    <Typography variant="body2" className="center">
+                        Cloudy
+                    </Typography>
+                </CardContent>
+            </Card>
+        </div>
+    )
 }
+
+export default ForecastCard
--- src/components/body/TemperatureForecast.js
+++ src/components/body/TemperatureForecast.js
@@ -1,7 +1,81 @@
 import React from 'react'
+import ForecastCard from './ForecastCard'
+import Grid from '@mui/material/Unstable_Grid2'
+import Paper from '@mui/material/Paper'
 
-const TemperatureForecast = () => {
-    return <div></div>
+import Toolbar from '@mui/material/Toolbar'
+import Button from '@mui/material/Button'
+import ButtonGroup from '@mui/material/ButtonGroup'
+import ToggleButton from '@mui/material/ToggleButton'
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup'
+
+const TemperatureForecast = (props) => {
+    const [value, setValue] = React.useState(0)
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue)
+    }
+
+    return (
+        <div className="temperature-forecast">
+            <Paper>
+                <Toolbar disableGutters>
+                    <h2>Forecast</h2>
+                    <ToggleButtonGroup
+                        value={value}
+                        exclusive
+                        onChange={handleChange}
+                        aria-label="text alignment"
+                    >
+                        <ToggleButton value="left" aria-label="left aligned">
+                            Temperature
+                        </ToggleButton>
+                        <ToggleButton value="center" aria-label="centered">
+                            Feels Like
+                        </ToggleButton>
+                    </ToggleButtonGroup>
+                </Toolbar>
+                <Grid container spacing={1}>
+                    <Grid xs={3} md={2} lg={1}>
+                        <ForecastCard />
+                    </Grid>
+                    <Grid xs={3} md={2} lg={1}>
+                        <ForecastCard />
+                    </Grid>{' '}
+                    <Grid xs={3} md={2} lg={1}>
+                        <ForecastCard />
+                    </Grid>{' '}
+                    <Grid xs={3} md={2} lg={1}>
+                        <ForecastCard />
+                    </Grid>{' '}
+                    <Grid xs={3} md={2} lg={1}>
+                        <ForecastCard />
+                    </Grid>
+                    <Grid xs={3} md={2} lg={1}>
+                        <ForecastCard />
+                    </Grid>{' '}
+                    <Grid xs={3} md={2} lg={1}>
+                        <ForecastCard />
+                    </Grid>{' '}
+                    <Grid xs={3} md={2} lg={1}>
+                        <ForecastCard />
+                    </Grid>{' '}
+                    <Grid xs={3} md={2} lg={1}>
+                        <ForecastCard />
+                    </Grid>{' '}
+                    <Grid xs={3} md={2} lg={1}>
+                        <ForecastCard />
+                    </Grid>
+                    <Grid xs={3} md={2} lg={1}>
+                        <ForecastCard />
+                    </Grid>
+                    <Grid xs={3} md={2} lg={1}>
+                        <ForecastCard />
+                    </Grid>
+                </Grid>
+            </Paper>
+        </div>
+    )
 }
 
 export default TemperatureForecast
